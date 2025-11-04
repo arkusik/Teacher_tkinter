6f9353cd19611efb13f6174cf8aa9e5ea6ba1d89
arkusik <a40704968@gmail.com>
2025-11-04 17:43:34 +0300

    print qwe

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 from tkinter import *   
 from konspect import *
     
+
+
 def main():
     root = Tk() 
     root.attributes("-fullscreen", False)
     konspect_ = Button(text= "Конспекты", command=go_konsp, width=10, height=5)
     konspect_.place(x = 150, y = 600)
     root.mainloop()
-
+    print('qwer')
 main()
